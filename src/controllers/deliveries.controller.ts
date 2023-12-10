3e4804f005a42336a9af15de8ceac0c7eada2d30
Florian Perrier <fperrier@wedigital.garden>
2023-12-11 00:57:44 +0100

    send notification to nearby couriers when a delivery is created

--- src/controllers/deliveries.controller.ts
+++ src/controllers/deliveries.controller.ts
@@ -1,5 +1,8 @@
 import FirebaseAdmin from '@/config/firebaseAdmin';
 import { CreateDeliveryDto, UpdateDeliveryDto } from '@/dtos/deliveries.dto';
+import { Roles } from '@/enums/roles.enum';
+import { HttpException } from '@/exceptions/HttpException';
+import { RequestWithUser } from '@/interfaces/auth.interface';
 import Courier from '@/models/couriers.model';
 import Delivery from '@/models/deliveries.model';
 import User from '@/models/users.model';
@@ -17,6 +20,40 @@ export class DeliveryController {
     try {
       const deliveries: Delivery[] = await this.deliveryService.findAllDeliveries();
 
+      res.status(200).json(deliveries);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getClientDeliveries = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const clientId = Number(req.params.clientId);
+      if (req.user.role !== Roles.ADMIN) {
+        if (req.user.id !== clientId) throw new HttpException(403, 'Access denied');
+      }
+      const client = await User.findByPk(clientId);
+      if (!client) throw new HttpException(404, 'Client not found');
+      const deliveries: Delivery[] = await Delivery.findAll({
+        where: { clientId: clientId },
+      });
+      res.status(200).json(deliveries);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getCourierDeliveries = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const courierId = Number(req.params.courierId);
+      const courier = await Courier.findByPk(courierId);
+      if (req.user.role !== Roles.ADMIN) {
+        if (req.user.id !== courier.userId) throw new HttpException(403, 'Access denied');
+      }
+      if (!courier) throw new HttpException(404, 'Courier not found');
+      const deliveries: Delivery[] = await Delivery.findAll({
+        where: { courierId: courierId },
+      });
       res.status(200).json(deliveries);
     } catch (error) {
       next(error);
@@ -43,6 +80,7 @@ export class DeliveryController {
         [data.dropoffLongitude, data.dropoffLatitude] = dropoffLocation;
       }
       const delivery: Delivery = await this.deliveryService.createDelivery(data);
+      this.sendNewDeliveryNotification(delivery.id);
       res.status(201).json(delivery);
     } catch (error) {
       next(error);
@@ -72,47 +110,43 @@ export class DeliveryController {
     }
   };
 
-  public sendNewDeliveryNotification = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const id = Number(req.params.id);
-      const delivery: Delivery = await Delivery.findByPk(id, {
+  private sendNewDeliveryNotification = async (delivery: Delivery | number) => {
+    if (typeof delivery === 'number') {
+      delivery = await Delivery.findByPk(delivery, {
         include: [{ model: User, as: 'client', attributes: ['firstName', 'lastName'] }],
       });
-      const nearbyCouriers = await Courier.findAll({
-        where: Sequelize.literal(
-          `ST_Distance(ST_MakePoint(longitude, latitude)::geography, ST_MakePoint(${delivery.pickupLongitude}, ${delivery.pickupLatitude})::geography) <= 15000000
+    }
+    const nearbyCouriers = await Courier.findAll({
+      where: Sequelize.literal(
+        `ST_Distance(ST_MakePoint(longitude, latitude)::geography, ST_MakePoint(${delivery.pickupLongitude}, ${delivery.pickupLatitude})::geography) <= 15000000
           AND status = 'available'
           `,
-        ),
-        include: [
-          {
-            model: User,
-            attributes: ['notificationToken'],
-            where: { notificationToken: { [Op.ne]: null } },
-          },
-        ],
-        attributes: ['id'],
-      });
-      const message = {
-        notification: {
-          title: 'Demande de livraison',
-          body: `Nouvelle livraison de ${getDistanceInMeters(
-            delivery.pickupLatitude,
-            delivery.pickupLongitude,
-            delivery.dropoffLatitude,
-            delivery.dropoffLongitude,
-          )} mètres près de votre position`,
+      ),
+      include: [
+        {
+          model: User,
+          attributes: ['notificationToken'],
+          where: { notificationToken: { [Op.ne]: null } },
         },
-        data: {
-          click_action: 'OPEN_DELIVERY_DETAILS',
-          deliveryId: delivery.id.toString(),
-        },
-        tokens: nearbyCouriers.map(courier => courier.user.notificationToken),
-      };
-      await FirebaseAdmin.getInstance().getMessaging().sendMulticast(message);
-      res.status(200).json(delivery);
-    } catch (error) {
-      next(error);
-    }
+      ],
+      attributes: ['id'],
+    });
+    const message = {
+      notification: {
+        title: 'Demande de livraison',
+        body: `Nouvelle livraison de ${getDistanceInMeters(
+          delivery.pickupLatitude,
+          delivery.pickupLongitude,
+          delivery.dropoffLatitude,
+          delivery.dropoffLongitude,
+        )} mètres près de votre position`,
+      },
+      data: {
+        click_action: 'OPEN_DELIVERY_DETAILS',
+        deliveryId: delivery.id.toString(),
+      },
+      tokens: nearbyCouriers.map(courier => courier.user.notificationToken),
+    };
+    await FirebaseAdmin.getInstance().getMessaging().sendMulticast(message);
   };
 }
