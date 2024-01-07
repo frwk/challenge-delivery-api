60bf6bd3a1e7df334e4d1d9d235926208efe2969
Florian Perrier <florian.fp.perrier@gmail.com>
2024-01-07 20:17:16 +0100

    remove old route

--- src/routes/deliveries.route.ts
+++ src/routes/deliveries.route.ts
@@ -54,6 +54,5 @@ export class DeliveryRoute implements Routes {
       AuthMiddleware(Roles.ADMIN, Roles.COURIER),
       this.deliveryController.getCourierCurrentDelivery,
     );
-    this.router.get(`${this.path}/estimate`, this.deliveryController.getEstimatedDeliveryTime);
   }
 }
